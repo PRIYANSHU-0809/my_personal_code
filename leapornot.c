fde891025902f30c4c3ae2454ad0b8676397cb25
Priyanshu <kpriyanshu272@gmail.com>
2021-10-23 18:48:27 +0530

    leap or not file added

---
+++ leapornot.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+
+
+{
+int year;
+ printf("Enter the year .\n");
+ scanf("%d", &year);
+ if (year%4==0)
+ {
+           printf("Entered year is leap.\n");
+ }
+ else{
+           printf("Entered year is not a leap.");
+ }
+ return 0;
+
+}
