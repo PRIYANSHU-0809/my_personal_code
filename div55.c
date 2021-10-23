fecb5f7d56549e21ad8bfb2e1631575fe200024f
Priyanshu <kpriyanshu272@gmail.com>
2021-10-23 12:32:19 +0530

    div55.c added

---
+++ div55.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int num;
+
+printf("Enter any number to check whether it is divisible by 55 or not.\n");
+
+scanf("%d",&num );
+
+if(num%55==0)
+{
+          printf("Number entered by user is divisible by 55.\n");
+
+}else{
+
+
+printf("Number entered is not divisible by 55.\n");
+}
+return 0;
+
+}
