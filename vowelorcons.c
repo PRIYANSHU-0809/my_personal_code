c897f34109c5e05d9392da9609f50fd412bea17f
Priyanshu <kpriyanshu272@gmail.com>
2021-10-24 16:42:01 +0530

    vowelorcons.c added

---
+++ vowelorcons.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char sample,sym;
+printf("Enter any alphabet.\n");
+scanf("%c",&sample);
+
+if(sym=='a'|| sym=='e'|| sym=='i'||sym=='o'||sym=='u',sym=='A'|| sym=='E'|| sym=='I'||sym=='O'||sym=='U')
+{
+          printf("The input is vowel.\n");
+
+}
+else{
+          printf("The input is consonant.");
+}
+return 0;
+
+}
