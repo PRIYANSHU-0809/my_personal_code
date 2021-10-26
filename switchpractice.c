709626526b2c5b6ce9f8fa61a44a83b1a2922562
Priyanshu <kpriyanshu272@gmail.com>
2021-10-26 08:30:28 +0530

    file which act as a substitute of if elseif condition

---
+++ switchpractice.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{int rat;
+printf("Enter your experience with this hotel with the rating of 1-5.\n");
+scanf("%d", &rat);
+
+switch(rat){
+case 1: printf("Thanks for your feedback.Your experience is bad.\n");
+break;
+case 2: printf("Thanks for your feedback.Your experience is little bit good.\n");
+break;
+case 3: printf("Thanks for your feedback.Your experience is good. \n");
+break;
+case 4: printf("Thanks for your feedback.Your experience is very much good.\n");
+break;
+case 5: printf("Thanks for your feedback.Your experience is amazing.\n");
+break;
+
+default: printf("Your rating is invalid.You have not given the rating between 1-5.");
+
+
+}
+return 0;
+
+}
