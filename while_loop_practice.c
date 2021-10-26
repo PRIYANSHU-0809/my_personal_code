13b9c9f5603d950af2d89f5ce43730cbc17084a1
Priyanshu <kpriyanshu272@gmail.com>
2021-10-26 20:26:50 +0530

    new practice code added of while loop

---
+++ while_loop_practice.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+          int a;
+          printf("Enter the value of a.\n");
+          scanf("%d", &a);
+
+          while (a<15)
+          {
+
+                    printf("The value of a is %d .\n", a);
+                    a++;
+          }
+
+          return 0;
+          
+}
