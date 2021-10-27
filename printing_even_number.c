67598a4b21773db2a44b91ffd61e4c210f0bf7b4
Priyanshu <kpriyanshu272@gmail.com>
2021-10-27 08:01:08 +0530

    program which print even number from 1 to 1000

---
+++ printing_even_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+          int a;
+printf("Program to print even numbers between 1 to 1000.\n");
+printf("For printing the required values press 2.\n");
+scanf("%d", &a);
+
+          while(a<=1000){
+          printf("The value of a is %d.\n", a);
+
+          a+=2;
+
+
+
+          }
+}
