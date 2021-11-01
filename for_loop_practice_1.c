29ff7aa8e10e8c209bd994fd2b4a128eea160cf6
Priyanshu <kpriyanshu272@gmail.com>
2021-11-01 08:05:50 +0530

    program print n natural number using for loop

---
+++ for_loop_practice_1.c
@@ -0,0 +1,20 @@
+//program which print n natural numbers using for loop
+
+#include<stdio.h>
+int main()
+{
+int i, n;
+printf("Enter the value of n.\n");
+scanf("%d",&n );
+
+for (i=0; i<n; i++)
+{
+
+          printf("%d\n", i+1);
+
+}
+return 0;
+
+
+
+}
