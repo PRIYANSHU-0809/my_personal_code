976b93c90a51e0d79e17c826e6baf7dab081c0b4
Priyanshu <kpriyanshu272@gmail.com>
2021-11-02 07:35:35 +0530

    program which print sum of first 10 natural numbers using while loop : demostration of usage of while loop

---
+++ sum_natural_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+int n=1, sum=0;
+
+
+while(n<=10)
+{
+sum=sum+n;
+n++;
+
+}
+printf("The sum of first 10 natural number using while loop is :%d", sum);
+
+return 0;
+
+
+}
